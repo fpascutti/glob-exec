03612fb1161f1bd93af7f7f685a233f256cd97c1
Franck Pascutti <franck.pascutti@thomsonreuters.com>
2017-04-24 14:07:29 +0200

    Added `foreach` to command line interface.

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,34 @@
 #!/usr/bin/env node
 
 import { execSync } from "child_process";
-import { all } from "./glob-exec";
+import { all, foreach } from "./glob-exec";
 
 // tslint:disable-next-line:no-var-requires
 const subarg: any = require("subarg"); // no type definition for `subarg` yet
 
-const argv: any = subarg(process.argv.slice(2));
-const pattern: string = argv._.shift();
-all(pattern, argv._.join(" "), argv.glob).then((cmd: string): Buffer => {
-  return execSync(cmd, { stdio: "inherit" });
-}).then(undefined, (err: any): void => {
-  throw err;
+const argv: any = subarg(process.argv.slice(2), {
+  boolean: ["foreach"]
 });
+
+const pattern: string = argv._.shift();
+const command: string = argv._.join(" ");
+
+if (argv.foreach) {
+
+  foreach(pattern, command, argv.glob).then((cmds: string[]): void => {
+    cmds.forEach((cmd: string): void => {
+      execSync(cmd, { stdio: "inherit" });
+    });
+  }).then(undefined, (err: any): void => {
+    throw err;
+  });
+
+} else {
+
+  all(pattern, command, argv.glob).then((cmd: string): Buffer => {
+    return execSync(cmd, { stdio: "inherit" });
+  }).then(undefined, (err: any): void => {
+    throw err;
+  });
+
+}
